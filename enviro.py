8dd6e7fe91f4034c8a89dc2f4b362c80e46473a3
mark <mark@mrodman.com>
2019-12-28 14:12:26 +0000

    ff

--- enviro.py
+++ enviro.py
@@ -1,5 +1,7 @@
 # Environment variables
 
 # Elastic Client node
+e_client_proto = "http://"
 e_client_ip = "192.168.1.29"
 e_client_port = "9200"
+e_target = e_client_proto + e_client_ip + ":"+ e_client_port
--- test.py
+++ test.py
@@ -1,4 +1,45 @@
 #! /usr/bin/env python3
 #
 
-import elasticsearch
+from elasticsearch import *
+from enviro import *
+import requests
+import pprint
+import json
+
+
+def check_client_requests(target):
+    res = requests.get(target)
+    if res.status_code != 200:
+        print("Error connecting!!")
+
+    print("Connected to Elastic Client Node: "+ target)
+    return res.status_code
+
+
+def check_health_es(target, port):
+    pp = pprint.PrettyPrinter(indent=4)
+    es = Elasticsearch([{'host': target, 'port': port}])
+    e_health = (es.cluster.health())
+    pp.pprint(e_health)
+    if (e_health.get('status')).lower() != "green":
+        print("\n !!! Warning cluster is not optimal, please seek review !!!")
+        if (e_health.get('status')).lower() == "red":
+            print("Cluster status is in the red, urgent")
+        print(" !!! Status is :" + e_health.get('status') + "                                  !!!")
+
+
+
+
+def main():
+    cluster_status_code = check_client_requests(e_target)
+    if cluster_status_code == 200:
+        print("we connected checking health")
+        check_health_es(e_client_ip, e_client_port)
+    else:
+        print("Exiting, we should sent a P1 alert")
+        exit()
+
+
+if __name__ == '__main__':
+    main()
